746ecef832f89d253a16707ce6ea4aa491d9e97a
Yusuf Oyinlola <yuzstackinnovation@gmail.com>
2026-01-03 08:58:32 +0100

    Implement props

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
-import React from "react";
-import pizzaData from "./data";
+import React from 'react';
+import pizzaData from './data';
+import './index.css';
 
-function Pizza() {
+function Pizza({ photoName, name, ingredients, price }) {
   return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="spinaci" />
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    <div className='pizza'>
+      <img src={photoName} alt={name} />
+      <div>
+        <h3>{name}</h3>
+        <p>{ingredients}</p>
+        <span>${price}</span>
+      </div>
     </div>
   );
 }
 
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
+  return (
+    <header className='header'>
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <div>
+    <div className='menu'>
       <h2>Our menu:</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <div className='pizzas'>
+        {pizzaData.map((pizza, index) => (
+          <Pizza key={index} {...pizza} />
+        ))}
+      </div>
     </div>
   );
 }
@@ -31,8 +41,17 @@ function Main({ children }) {
 }
 
 function Footer() {
+  const date = new Date();
+  const hour = date.getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
   return (
-    <footer>{new Date().toLocaleTimeString()}. We're current open!</footer>
+    <footer className='footer'>
+      {date.toLocaleTimeString()}.{' '}
+      {`${isOpen ? "We're currently open!" : "Sorry, we're close."}`}
+    </footer>
   );
 
   // return React.createElement("footer", null, "We're current open!");
@@ -40,13 +59,13 @@ function Footer() {
 
 function App() {
   return (
-    <>
+    <div className='container'>
       <Main>
         <Header />
         <Menu />
       </Main>
       <Footer />
-    </>
+    </div>
   );
 }
 
