8fbbad7f7a9d75a6e25f6b3734cff69018e85499
Yusuf Oyinlola <yuzstackinnovation@gmail.com>
2026-01-04 05:11:11 +0100

    Implement more concepts...
    Implement Fragments, and Setting Classes & Text Conditonally.

--- src/App.jsx
+++ src/App.jsx
@@ -31,30 +31,36 @@ function Menu() {
   const numPizzas = pizzas.length;
 
   return (
-    <div className='menu'>
+    <section className='menu'>
       <h2>Our menu:</h2>
 
       {numPizzas > 0 ? (
-        <ul className='pizzas'>
-          {pizzas.map((pizza, index) => (
-            <Pizza key={index} {...pizza} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className='pizzas'>
+            {pizzas.map((pizza, index) => (
+              <Pizza key={index} {...pizza} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later :)</p>
       )}
-    </div>
+    </section>
   );
 }
 
-function Pizza({ photoName, name, ingredients, price }) {
+function Pizza({ photoName, name, ingredients, price, soldOut }) {
   return (
-    <li className='pizza'>
+    <li className={`pizza ${soldOut ? 'sold-out' : ''}`}>
       <img src={photoName} alt={name} />
       <div>
         <h3>{name}</h3>
         <p>{ingredients}</p>
-        <span>${price}</span>
+        <span>{soldOut ? 'SOLD OUT' : `$${price}`}</span>
       </div>
     </li>
   );
@@ -63,32 +69,29 @@ function Pizza({ photoName, name, ingredients, price }) {
 function Footer() {
   const date = new Date();
   const hour = date.getHours();
-  const openHour = 19;
+  const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
 
   return (
     <footer className='footer'>
-      {/* Conditional Rendering with Short-Circuting (&&) */}
-      {/* {isOpen && (
-        <div className='order'>
-          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-          <button className='btn'>Order</button>
-        </div>
-      )} */}
-
-      {/* Conditional Rendering with Ternay Operator */}
       {isOpen ? (
-        <div className='order'>
-          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-          <button className='btn'>Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           W're happy to welcome you between {openHour}:00 and {closeHour}:00.
         </p>
       )}
     </footer>
+  );
+}
+
+function Order({ closeHour }) {
+  return (
+    <div className='order'>
+      <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+      <button className='btn'>Order</button>
+    </div>
   );
 }
 
